e26cb7b01616c68969f1cc1672009cc994f5b323
Remy-Oliveira <remy.musico@gmail.com>
2024-10-01 13:37:21 -0300

    update

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ import "./src/Styles/generic/reset.css"
 import "./src/Styles/elements/base.css";
 
 import CardGame from "./src/components/CardGame";
+import Players from "./src/components/Players";
 
 //${CardGame().repeat(6)}
 
 document.querySelector("#root").insertAdjacentHTML("beforeend", CardGame());
+
+document.querySelector("#root").insertAdjacentHTML("beforeend", Players('Remy!'));
---
+++ src/components/Players/index.js
@@ -0,0 +1,5 @@
+import "./style.css";
+
+const Players = (players) => `<p class="paragrafo">${players} </p>`;
+
+export default Players;
