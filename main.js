4f2c807b96451c60fd8939b01a44b1d7464d4e91
Remy-Oliveira <remy.musico@gmail.com>
2024-10-01 10:44:53 -0300

    Created

---
+++ main.js
@@ -0,0 +1,8 @@
+import "./src/Styles/settings/colors.css";
+import "./src/Styles/generic/reset.css"
+import "./src/Styles/elements/base.css";
+
+import CardGame from "./src/components/CardGame";
+("./src/components/CardGame");
+
+document.querySelector("#root").insertAdjacentHTML("beforeend", CardGame());
---
+++ src/components/CardGame/index.js
@@ -0,0 +1,10 @@
+import "./style.css";
+
+const CardGame = () => `
+<article class="card-game">
+    <img src = "images/alura-pixel.png" alt= "Logo da Alura!">
+</article>`;
+
+export default CardGame;
+
+// export {CardGame};
