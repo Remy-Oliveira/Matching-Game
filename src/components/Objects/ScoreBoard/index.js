3b093eb7416e1513954647a09dc65de87728288c
Remy-Oliveira <remy.musico@gmail.com>
2024-10-02 11:03:01 -0300

    justify-content

--- src/components/Objects/ScoreBoard/index.js
+++ src/components/Objects/ScoreBoard/index.js
@@ -1,8 +1,10 @@
 import PlayerName from "../../PlayerName/";
-
+import "./style.css";
 const ScoreBoard = () => {
-  return  `<header class="score-board"> ${PlayerName("Player1" + "Player2")} </header>`;
+  return `<header class="score-board">
+   ${PlayerName("Player1")} 
+   ${PlayerName("Player2")}
+   </header>`;
 };
 
 export default ScoreBoard;
-
