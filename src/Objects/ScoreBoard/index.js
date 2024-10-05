e45414cc92c42d906af41c6e36f95534b603a040
Remy-Oliveira <remy.musico@gmail.com>
2024-10-05 13:28:04 -0300

    Update index.js

--- src/Objects/ScoreBoard/index.js
+++ src/Objects/ScoreBoard/index.js
@@ -4,7 +4,12 @@ import VsPlayer from "../../components/VsPlayer";
 import "./style.css";
 
 const ScoreBoard = () => {
-  return `<header class="score-board"> ${PlayerName("Player1")} ${PlayerScore(2)} ${VsPlayer()}${PlayerScore(1)} ${PlayerName("Player2")} </header>`;
+  return `<header class="score-board"> 
+  ${PlayerName("Player1")} 
+  ${PlayerScore(2)}
+  ${VsPlayer()}
+  ${PlayerScore()}
+  ${PlayerName("Player2")}</header>`;
 };
 
 export default ScoreBoard;
