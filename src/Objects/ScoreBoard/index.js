a8a0ea03d94146566bdbd9653e909b29df743c4c
Remy-Oliveira <remy.musico@gmail.com>
2024-10-05 13:16:46 -0300

    ol created

--- src/Objects/ScoreBoard/index.js
+++ src/Objects/ScoreBoard/index.js
@@ -1,9 +1,10 @@
 import PlayerName from "../../components/PlayerName";
+import PlayerScore from "../../components/PlayerScore";
 import VsPlayer from "../../components/VsPlayer";
 import "./style.css";
 
 const ScoreBoard = () => {
-  return `<header class="score-board"> ${PlayerName("Player1")} ${VsPlayer()} ${PlayerName("Player2")} </header>`;
+  return `<header class="score-board"> ${PlayerName("Player1")} ${PlayerScore(2)} ${VsPlayer()}${PlayerScore(1)} ${PlayerName("Player2")} </header>`;
 };
 
 export default ScoreBoard;
